7e4a885c324835b3e32c5f52dab7da0b4de68b90
hungvipsl030599 <hungvipsl111@gmail.com>
2020-12-31 17:27:47 +0700

    hung

---
+++ app/src/main/java/com/example/noname/TrangBenhNhan/BenhNhan_DoiMatKhau.java
@@ -0,0 +1,47 @@
+package com.example.noname.TrangBenhNhan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.example.noname.R;
+import com.example.noname.TrangBenhNhan.Fragment.Fragment_BenhNhan_HoSoBenhNhan;
+
+public class BenhNhan_DoiMatKhau extends AppCompatActivity {
+
+    ImageButton imgBack;
+    Button btnCapNhat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_benh_nhan__doi_mat_khau);
+        controls();
+        events();
+
+    }
+
+    private void events() {
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BenhNhan_DoiMatKhau.this, Fragment_BenhNhan_HoSoBenhNhan.class));
+            }
+        });
+        btnCapNhat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BenhNhan_DoiMatKhau.this, BenhNhan_DangNhapBenhNhan.class));
+            }
+        });
+    }
+
+    private void controls() {
+        imgBack=findViewById(R.id.imgBack_BenhNhan_DoiMatKhau);
+        btnCapNhat=findViewById(R.id.btnCapNhat_BenhNhan_DoiMatKhau);
+    }
+}
